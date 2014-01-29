3d23ebd64e13bdb32c0ced775937e351dbb183f8
David Wells <wellsland@gmail.com>
2014-01-28 19:23:32 -0800

    TieBreaker Progress
    Working on the tiebreaker class

--- Tennis Object Oriented/TieBreaker.cpp
+++ Tennis Object Oriented/TieBreaker.cpp
@@ -7,9 +7,37 @@
 //
 
 #include "TieBreaker.hpp"
+#include "TieBreakerScore.cpp"
+#include "Score.hpp"
+#include "GameScore.hpp"
+#include "PointScore.hpp"
 
 TieBreaker::TieBreaker( Player *p1, Player *p2 ): Competition( p1, p2 ) {}
+
 Score *TieBreaker::play( Player *p ) {
-    return 0;
+    
+    
+    Score *score = new GameScore( player1(), player2() );
+    
+    while( !score->haveAWinner() ) {
+        PointScore *pointScore = reinterpret_cast<PointScore *>( p->serveAPoint() );
+        score->addScore( pointScore->getWinner() );
+        delete pointScore;
+    }
+    return score;
+    
+    
+    
+    // Original C++ code:
+    /*
+     p1_score = p2_score = 0;
+     point( player ) == PLAYER1 ? p1_score++ : p2_score++;
+     int switchServe = 0;
+     do {
+     if( switchServe++ % 2 == 0 ) player = other(player);
+     point( player ) == PLAYER1 ? p1_score++ : p2_score++;
+     } while ( ! tie_over( p1_score, p2_score ) );
+     return p1_score > p2_score ? PLAYER1 : PLAYER2;
+     */
 }
 
